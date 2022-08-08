3acf5598851cf399587288551758928cc17aac49
Chris Martin <ch.martin@gmail.com>
2022-08-08 15:17:00 -0600

    fix Positive 'minus' function

--- nat-optics/src/NatOptics/Positive/Math.hs
+++ nat-optics/src/NatOptics/Positive/Math.hs
@@ -7,13 +7,10 @@ module NatOptics.Positive.Math
   )
   where
 
-import Data.Maybe          ( Maybe (..) )
-import Data.Ord            ( Ord, (<) )
-import Optics.Core         ( preview, review )
-import Prelude             ( Num, abs, (+), (-) )
+import Data.Ord            ( Ord, compare, Ordering (..) )
+import Prelude             ( Num, (+), (-) )
 
 import NatOptics.Positive.Unsafe ( Positive (..) )
-import qualified NatOptics.Positive as Positive
 
 import NatOptics.Signed ( Signed )
 import qualified NatOptics.Signed as Signed
@@ -22,12 +19,8 @@ plus :: Num n => Positive n -> Positive n -> Positive n
 plus (PositiveUnsafe a) (PositiveUnsafe b) = PositiveUnsafe (a + b)
 
 minus :: (Num n, Ord n) => Positive n -> Positive n -> Signed n
-minus a b =
-    let
-        a' = review Positive.refine a
-        b' = review Positive.refine b
-        diff = a' - b'
-    in
-        case preview Positive.refine (abs diff) of
-            Nothing -> Signed.Zero
-            Just diff' -> if diff < 0 then Signed.Minus diff' else Signed.Plus diff'
+minus (PositiveUnsafe a) (PositiveUnsafe b) =
+    case compare a b of
+        EQ -> Signed.Zero
+        GT -> Signed.Plus (PositiveUnsafe (a - b))
+        LT -> Signed.Minus (PositiveUnsafe (b - a))
