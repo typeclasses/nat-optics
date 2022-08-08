4cfbbab4c4a4159be91568037964adb29f383461
Chris Martin <ch.martin@gmail.com>
2022-08-08 15:17:45 -0600

    add Positive 'absoluteDifference'

--- nat-optics/src/NatOptics/Positive/Math.hs
+++ nat-optics/src/NatOptics/Positive/Math.hs
@@ -4,16 +4,19 @@ module NatOptics.Positive.Math
   (
     minus,
     plus,
+    absoluteDifference,
   )
   where
 
-import Data.Ord            ( Ord, compare, Ordering (..) )
+import Data.Ord            ( Ord, (<), compare, Ordering (..) )
 import Prelude             ( Num, (+), (-) )
 
 import NatOptics.Positive.Unsafe ( Positive (..) )
 
 import NatOptics.Signed ( Signed )
 import qualified NatOptics.Signed as Signed
+
+import NatOptics.NonNegative.Unsafe ( NonNegative (..) )
 
 plus :: Num n => Positive n -> Positive n -> Positive n
 plus (PositiveUnsafe a) (PositiveUnsafe b) = PositiveUnsafe (a + b)
@@ -24,3 +27,7 @@ minus (PositiveUnsafe a) (PositiveUnsafe b) =
         EQ -> Signed.Zero
         GT -> Signed.Plus (PositiveUnsafe (a - b))
         LT -> Signed.Minus (PositiveUnsafe (b - a))
+
+absoluteDifference :: (Num n, Ord n) => Positive n -> Positive n -> NonNegative n
+absoluteDifference (PositiveUnsafe a) (PositiveUnsafe b) =
+    NonNegativeUnsafe (if a < b then b - a else a - b)
