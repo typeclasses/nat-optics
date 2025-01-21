a6fc817529a2879503960452b60002af2fea600e
Chris Martin <ch.martin@gmail.com>
2025-01-21 00:55:28 -0700

    version bumps

--- flake.nix
+++ flake.nix
@@ -1,16 +1,13 @@
 {
   inputs = {
-    "nixos-22.11".url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = inputs@{ self, ... }:
     let packageName = "nat-optics";
     in inputs.flake-utils.lib.eachDefaultSystem (system:
       let
-        nixpkgs = {
-          "nixos-22.11" = import inputs."nixos-22.11" { inherit system; };
-        };
-        pkgs = nixpkgs."nixos-22.11";
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
         project = pkgs.haskellPackages.developPackage {
           root = ./nat-optics;
           name = packageName;
@@ -23,41 +20,32 @@
       in {
         defaultPackage = self.packages.${system}.${packageName};
 
-        packages = {
+        packages = let
+
+          inherit (pkgs.haskell.lib) dontCheck;
+
+          makeTestConfiguration = let defaultPkgs = pkgs;
+          in { pkgs ? defaultPkgs, ghcVersion, overrides ? new: old: { } }:
+          let inherit (pkgs.haskell.lib) dontCheck packageSourceOverrides;
+          in (pkgs.haskell.packages.${ghcVersion}.override (old: {
+            overrides = combineOverrides old [
+              (packageSourceOverrides { nat-optics = ./nat-optics; })
+              overrides
+            ];
+
+          })).nat-optics;
+
+        in rec {
           "${packageName}" = project;
 
-          testConfigurations = let
-
-            inherit (pkgs.haskell.lib) dontCheck;
-
-            makeTestConfiguration = let defaultPkgs = pkgs;
-            in { pkgs ? defaultPkgs, ghcVersion, overrides ? new: old: { } }:
-            let inherit (pkgs.haskell.lib) dontCheck packageSourceOverrides;
-            in (pkgs.haskell.packages.${ghcVersion}.override (old: {
-              overrides = combineOverrides old [
-                (packageSourceOverrides { nat-optics = ./nat-optics; })
-                overrides
-              ];
-
-            })).nat-optics;
-
-          in rec {
-            ghc-9-0 = makeTestConfiguration {
-              pkgs = nixpkgs."nixos-22.11";
-              ghcVersion = "ghc90";
-            };
-            ghc-9-2 = makeTestConfiguration {
-              pkgs = nixpkgs."nixos-22.11";
-              ghcVersion = "ghc92";
-            };
-            ghc-9-4 = makeTestConfiguration {
-              pkgs = nixpkgs."nixos-22.11";
-              ghcVersion = "ghc94";
-            };
-            all = pkgs.symlinkJoin {
-              name = packageName;
-              paths = [ ghc-9-0 ghc-9-2 ghc-9-4 ];
-            };
+          ghc-9-0 = makeTestConfiguration { ghcVersion = "ghc90"; };
+          ghc-9-2 = makeTestConfiguration { ghcVersion = "ghc92"; };
+          ghc-9-4 = makeTestConfiguration { ghcVersion = "ghc94"; };
+          ghc-9-6 = makeTestConfiguration { ghcVersion = "ghc96"; };
+          ghc-9-8 = makeTestConfiguration { ghcVersion = "ghc98"; };
+          all = pkgs.symlinkJoin {
+            name = packageName;
+            paths = [ ghc-9-0 ghc-9-2 ghc-9-4 ghc-9-6 ghc-9-8 ];
           };
         };
       });
---
+++ nat-optics/changelog.md
@@ -0,0 +1,7 @@
+### 1.0.1.1 (2025-01-21)
+
+Version bumps
+
+### 1.0.1.0
+
+Start of change log
